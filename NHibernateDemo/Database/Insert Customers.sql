33d1b2447fc271133d15a6a939aa16a9c090d66c
ait27ru <ait27ru@gmail.com>
2016-11-13 23:22:25 +1300

    Demo - querying data.

---
+++ NHibernateDemo/Database/Insert Customers.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO [dbo].[Customer] VALUES
+(N'John', N'Smith'),
+(N'Joe' , N'Blogger'),
+(N'Deon', N'Fraser'),
+(N'Carl', N'Bergen'),
+(N'Leo' , N'Corten')
--- NHibernateDemo/Program.cs
+++ NHibernateDemo/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+using NHibernate.Linq;
+using NHibernate.Util;
 
 namespace NHibernateDemo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var cfg = new Configuration();
+
+            cfg.DataBaseIntegration(x =>
+            {
+                x.ConnectionStringName = "DemoConnection";
+                x.Driver<SqlClientDriver>();
+                x.Dialect<MsSql2012Dialect>();
+            });
+
+            cfg.AddAssembly(Assembly.GetExecutingAssembly());
+
+            var sessionFactory = cfg.BuildSessionFactory();
+
+            using (var session = sessionFactory.OpenSession())
+            using (var tx = session.BeginTransaction())
+            {
+                // get Customers using Criteria
+                var customers = session.CreateCriteria<Customer>()
+                    .List<Customer>();
+                customers.ForEach(c => Console.WriteLine($"{c.FirstName,-10} {c.LastName}"));
+
+                // get Customers using LINQ
+                Console.WriteLine();
+
+                var customers2 = from customer in session.Query<Customer>()
+                    orderby customer.LastName
+                    select customer;
+                customers2.ForEach(c => Console.WriteLine($"{c.FirstName,-10} {c.LastName}"));
+
+                tx.Commit();
+            }
+            Console.WriteLine("Press <ENTER> to exit...");
+            Console.ReadLine();
         }
     }
 }
